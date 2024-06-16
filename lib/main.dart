ff1008f79f878f878aa02c4a13b17976f8561bcd
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2024-06-16 14:36:20 +0200

    feat/flutter

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Animation Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(begin: 0, end: 300).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Simple Assignment', 
+        style:TextStyle(
+          color:Colors.white,
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Poppins',
+          letterSpacing: 1.5,
+          wordSpacing: 1.5,
+          )
+        ),
+        backgroundColor: Colors.blue,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) {
+            return Transform.translate(
+              offset: Offset(_animation.value, 0),
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
